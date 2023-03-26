748b28ed049e38c7e28abd703b63ea837584bf16
Veselina Sidova <vessy.sidova@gmail.com>
2023-03-26 02:34:02 +0200

    re-created app

--- animal-sightings/src/App.js
+++ animal-sightings/src/App.js
@@ -1,21 +1,9 @@
-import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
-import { Home } from "./components/Home/Home";
-
-import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-
-      {/* <main id="main-content">
-        <Routes>
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </main>
-
-      <Footer /> */}
+        <Header />
     </div>
   );
 }
--- animal-sightings/src/components/Header/Header.js
+++ animal-sightings/src/components/Header/Header.js
@@ -1,18 +1,20 @@
-import styles from './Header.module.css';
-
 export const Header = () => {
     return (
-        <section className=".navigation-section">
-            <nav className=".navigation">
-                <ul className=".nav-list">
-                    <li>Home</li>
-                    <li>Animals in Bulgaria</li>
-                    <li>Animal Sightings</li>
-                    <li>My Sighted Animals</li>
-                    <li>Login</li>
+        <section className="header-section">
+            <nav role="navigation" className="navbar">
+                <div className="navbar-header">
+                    <a className="navbar-brand" href="#"><span className="green">Animal Sightings</span> Bulgaria</a>
+                </div>
+                <ul className="navbar-nav nav">
+                    <li className="active"><a href="#">Home</a></li>
+                    <li><a href="#">Animals in Bulgaria</a></li>
+                    <li><a href="#">Animal Sightings</a></li>
+                    <li><a href="#">Login</a></li>
+                    <li><a href="#">Register</a></li>
+                    <li><a href="#">My Animal Records</a></li>
+                    <li><a href="#">My Animal Sightings</a></li>
                 </ul>
             </nav>
         </section>
-
     );
 };
